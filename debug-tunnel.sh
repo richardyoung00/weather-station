5ea5f4ae83c52d95867e4438e3013bffc7d2541c
Richard Young <richard.young00@gmail.com>
2020-12-04 19:03:06 +0200

    Hide scroll bars

---
+++ debug-tunnel.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ssh -L 0.0.0.0:9223:localhost:9222 localhost -N
--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,9 @@
 import './components/home-page.js'
 import './components/clock-display.js'
 import './components/indoor-sensors.js'
+
+const refreshButton = document.querySelector('.refresh-button ion-button')
+
+refreshButton.addEventListener('click', (e) => {
+    location.reload();
+})

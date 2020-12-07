8bc3f76b5c29b18ceca39848df8e0764559a86b6
Richard Young <richard.young00@gmail.com>
2020-12-07 15:35:11 +0200

    Backlight fading

--- services/backlight.js
+++ services/backlight.js
@@ -4,6 +4,7 @@ const SCREENSAVER_OFF = `xset -d :0 s off`
 const SCREEN_TIMEOUT_OFF = `xset -d :0 dpms 0 0 0`
 const SCREEN_ON = `xset -d :0 dpms force on`
 const SCREEN_OFF = `xset -d :0 dpms force off`
+const GET_BRIGHTNESS = `cat /sys/class/backlight/rpi_backlight/actual_brightness`
 const SET_SCREEN_TIMEOUT = (timeout_s) => `xset -d :0 dpms 0 0 ${timeout_s}`
 // value between 0 and 255
 const SET_SCREEN_BRIGHTNESS = (brightness_value) => `echo ${brightness_value} > /sys/class/backlight/rpi_backlight/brightness`
@@ -14,9 +15,14 @@ const MIN_LUX_SQ = 2; // min achievable lux
 const SCREEN_OFF_LUX_SQ = 2; 
 const NIGHT_TIME_SCREEN_TIMOUT_S = 5;
 
+const BRIGHTNESS_FADE_SPEED = 30;
+
 export class BacklightService {
     constructor(config) {
         this.config = config;
+        this.nightMode = false;
+        this.actualBrightness = 255;
+        this.targetBrightness = 255;
     }
 
     async exec(cmd) {
@@ -33,34 +39,44 @@ export class BacklightService {
         })
     }
 
+    async fadeBrightness() {
+        setInterval(async () => {
+            if (this.actualBrightness > this.targetBrightness) {
+                this.actualBrightness--
+                await this.exec(SET_SCREEN_BRIGHTNESS(this.actualBrightness))
+            } else if (this.actualBrightness < this.targetBrightness) {
+                this.actualBrightness++
+                await this.exec(SET_SCREEN_BRIGHTNESS(this.actualBrightness))
+            }
+        }, BRIGHTNESS_FADE_SPEED)
+    }
 
     async start() {
         await this.exec(SCREENSAVER_OFF)
         await this.exec(SCREEN_TIMEOUT_OFF)
         await this.exec(SCREEN_ON)
+        this.actualBrightness = parseInt(await this.exec(GET_BRIGHTNESS))
+        this.targetBrightness = this.actualBrightness;
+        this.fadeBrightness()
     }
 
     async setAmbientLight(ambient_light, lux)  {
         const lux_sq = Math.sqrt(lux)
-        console.log(lux_sq)
         const lux_sq_range = MAX_LUX_SQ - MIN_LUX_SQ
         let val = (lux_sq - MIN_LUX_SQ) / lux_sq_range * 255 
         val = Math.max(10, val)
         val = Math.min(val, 255)
         val = Math.round(val)
-        await this.exec(SET_SCREEN_BRIGHTNESS(val))
+        this.targetBrightness = val;
 
-        if (lux_sq <= SCREEN_OFF_LUX_SQ) {
-            console.log('GN')
-            //todo: need to keep track of if we already turned off screen, and not run this command again
-            // it resets the timer every time we run
-
+        if ( !this.nightMode && lux_sq <= SCREEN_OFF_LUX_SQ)  {
+            this.nightMode = true;
             const c = SET_SCREEN_TIMEOUT(NIGHT_TIME_SCREEN_TIMOUT_S)
-            console.log(c)
             await this.exec(SET_SCREEN_TIMEOUT(NIGHT_TIME_SCREEN_TIMOUT_S))
-        } else {
-            console.log('WK')
+        } else if (this.nightMode && lux_sq > SCREEN_OFF_LUX_SQ) {
+            this.nightMode = false;
             await this.exec(SCREEN_TIMEOUT_OFF)
+            await this.exec(SCREEN_ON)
         }
     }
 }

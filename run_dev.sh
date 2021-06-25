f0c6ea73b7bc91a0d094ffe5a495abd2db2df721
Richard Young <richard.young00@gmail.com>
2021-06-25 16:04:08 +0200

    Nodemon, exit button

--- run_dev.sh
+++ run_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-chromium-browser http://localhost:3000 \
+chromium-browser http://localhost:3001 \
   --user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1" \
   --window-size=1920,1080 \
   --window-position=0,0 \
